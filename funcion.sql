4ab02f3c0b8584bf2ebf278a58a27ce47b4638bd
jmunoz03 <149121982+jmunoz03@users.noreply.github.com>
2025-11-05 20:04:46 -0300

    Update funcion.sql

--- funcion.sql
+++ funcion.sql
@@ -1,20 +1,2 @@
-USE FreeSpaces;
-GO
 
 
--- Crea funcion que devuelve la cantidad de espacios segun el tipo 
-CREATE FUNCTION CantidadEspaciosPorTipo (@NombreTipo VARCHAR(100))
-RETURNS INT 
-BEGIN 
-    DECLARE @cantidad INT; --Crea una variable donde se va a guardar el resultado del conteo
-
-
-    SELECT @cantidad = COUNT(*)
-    FROM Espacios e
-    INNER JOIN TipoEspacio T On E.IDtipoEspacio =t.IDtipoEspacio
-    WHERE T.Nombre = @NombreTipo;
-
-    RETURN @cantidad;
-
-END;
-GO
