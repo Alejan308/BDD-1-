09abc22b0f60508d543e964361e4e1f71e4574cd
CrissJaimes <crissjaimes9@gmail.com>
2025-11-10 12:29:30 -0300

    Agregado de funcion para calcular duracion de estadia

--- funcion.sql
+++ funcion.sql
@@ -1,2 +1,45 @@
+USE FreeSpaces;
+GO
 
+CREATE FUNCTION fn_CalcularDuracionEstadia (
+    @HoraEntrada DATETIME,
+    @HoraSalida DATETIME
+)
+RETURNS VARCHAR(100) -- Devuelve un texto
+AS
+BEGIN
+    DECLARE @TotalMinutos INT;
+    DECLARE @Horas INT;
+    DECLARE @Minutos INT;
+    DECLARE @Resultado VARCHAR(100);
 
+    IF @HoraSalida IS NULL
+        SET @TotalMinutos = DATEDIFF(MINUTE, @HoraEntrada, GETDATE());
+    ELSE
+        SET @TotalMinutos = DATEDIFF(MINUTE, @HoraEntrada, @HoraSalida);
+
+    SET @Horas = @TotalMinutos / 60;
+    SET @Minutos = @TotalMinutos % 60;
+
+    -- pasamos del int a string con cast para imprimir el resultado
+    SET @Resultado = CAST(@Horas AS VARCHAR) + ' horas, ' + CAST(@Minutos AS VARCHAR) + ' minutos';
+
+    RETURN @Resultado;
+END;
+GO
+
+-- Para probarlo
+
+SELECT 
+    r.IDregistro,
+    r.IDusuario,
+    r.HoraEntrada,
+    r.HoraSalida,
+    -- ¡Aquí usamos la función!
+    dbo.fn_CalcularDuracionEstadia(r.HoraEntrada, r.HoraSalida) AS Duracion
+FROM 
+    Registro r
+WHERE
+    IDusuario = 3; 
+GO
+------------------------------------------------------------------------------------------------------------------------
